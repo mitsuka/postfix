0c8a5e82f00b7153c00a23213e4a222b38618726
MITSUKA Atsushi <a.mitsuka@gmail.com>
2019-06-27 18:08:55 +0900

    Add: exec newaliases

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ sed -i "s#%%relayhost%%#${RELAYHOST:-}#" /etc/postfix/main.cf
 sed -i "s#%%mynetwork%%#${MYNETWORK:-172.16.0.0/12}#" /etc/postfix/main.cf
 sed -i "s#^myhostname =.*\$#myhostname = ${MYHOSTNAME:-localhost.localdomain}#" /etc/postfix/main.cf
 sed -i "s#%%rootaddress%%#${ROOTADDRESS:-root@localhost}#" /etc/aliases
+newaliases
 /usr/sbin/rsyslogd
 sleep 3
 /usr/sbin/postfix start
